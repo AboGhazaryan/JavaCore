c0078312eac805545883a1502127b8a7dd83f161
AboGhazaryan <abo.ghazaryan@mail.ru>
2025-09-27 20:23:16 +0400

    homework5

--- src/homework/homework4/ArrayHomework.java
+++ src/homework/homework4/ArrayHomework.java
@@ -3,7 +3,7 @@ package homework.homework4;
 public class ArrayHomework {
     public static void main(String[] args) {
         // 1
-        int[] numbers = {1, 5, 6, 6, 7, 55, 4, 3, 15, 77};
+        int[] numbers = {1, 5, 6, 6, 7,7, 55, 4, 3, 15, 77};
         {
 //            int n = 6;
 //            {
@@ -17,40 +17,83 @@ public class ArrayHomework {
 //                System.out.print("Variable: " + quantity);
 //            }
 //            System.out.println();
-            //2
-
-
-            int lastindex = numbers.length - 1;
-            int temp;
-            for (int i = 0; i < numbers.length / 2; i++) {
-                temp = numbers[i];
-                numbers[i] = numbers[lastindex];
-                numbers[lastindex] = temp;
-                lastindex--;
-            }
-            for (int number : numbers) {
-                System.out.print(number + " ");
-            }
-            System.out.println();
+//            //2
+//
+//
+//            int lastindex = numbers.length - 1;
+//            int temp;
+//            for (int i = 0; i < numbers.length / 2; i++) {
+//                temp = numbers[i];
+//                numbers[i] = numbers[lastindex];
+//                numbers[lastindex] = temp;
+//                lastindex--;
+//            }
+//            for (int number : numbers) {
+//                System.out.print(number + " ");
+//            }
+//            System.out.println();
             //3
-            boolean[] checked = new boolean[numbers.length];
-            int repeatedCount = 0;
+//            boolean[] checked = new boolean[numbers.length];
+//            int repeatedCount = 0;
+//            for (int i = 0; i < numbers.length; i++) {
+//                if (checked[i]) {
+//                    continue;
+//                }
+//                int count = 1;
+//                for (int j = i + 1; j < numbers.length; j++) {
+//                    if (numbers[i] == numbers[j]) {
+//                        count++;
+//                        checked[j] = true;
+//                    }
+//                }
+//                if (count > 1) {
+//                    repeatedCount++;
+//                }
+//            }
+//            System.out.print("repeated number: " + repeatedCount);
+
+            System.out.println();
+//            int repeatablecount = 0;
+//            for (int i = 0; i < numbers.length; i++) {
+//                for (int j = i + 1; j < numbers.length - 1; j++) {
+//                    if(numbers[i] == numbers[j]){
+//                        repeatablecount ++;
+//                        break;
+//                    }
+//
+//                }
+//
+//            }
+//            System.out.println("repeatblecount:  " + repeatablecount);
+
+            int [] duplicateNumbers = new int [numbers.length];
+            int duplicateNumbersIndex = 0;
+            int repeatableCount = 0;
             for (int i = 0; i < numbers.length; i++) {
-                if (checked[i]) {
-                    continue;
-                }
-                int count = 1;
-                for (int j = i + 1; j < numbers.length; j++) {
-                    if (numbers[i] == numbers[j]) {
-                        count++;
-                        checked[j] = true;
+                for (int j = i + 1; j <numbers.length ; j++) {
+                    boolean dublicateFound = false;
+                    if (numbers[i] == numbers[j]){
+                        for (int dublicateNumber : duplicateNumbers){
+                            if  (numbers[i] == dublicateNumber){
+                                dublicateFound = true;
+                                break;
+                            }
+
+                        }
+                        if(!dublicateFound){
+                            repeatableCount++;
+                            duplicateNumbers[duplicateNumbersIndex++]= numbers[i];
+                        }
                     }
+
                 }
-                if (count > 1) {
-                    repeatedCount++;
-                }
+
             }
-            System.out.print("repeated number: " + repeatedCount);
+            System.out.println("repeatable count " + repeatableCount);
+            for (int i = 0; i < repeatableCount; i++) {
+                System.out.print(duplicateNumbers[i] + " ");
+
+            }
 
 
         }
--- src/leeson1/Example.java
+++ src/leeson1/Example.java
@@ -1,19 +1,35 @@
 package leeson1;
 
 public class Example {
-    public static void main(String[] args){
-      //int  num = 550;{
+    public static void main(String[] args) {
+        //int  num = 550;{
         //    System.out.println(num * 100);
-          //  num =50;
+        //  num =50;
         //}
 
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
-
+//        for (int i = 0; i <= 10; i++) {
+//            System.out.println(i);
+//
+//        }
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//        int val = 7;
+//        boolean found = false;
+//        for (int num : numbers){
+//            if (num == val){
+//                found = true;
+//                break;
+//            }
+//        }
+//        if (found){
+//            System.out.println(val);
+//        }
+        int val = 7;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == val) {
+                break;
+            }
         }
-
-
-
+        System.out.println(val);
 
 
     }
