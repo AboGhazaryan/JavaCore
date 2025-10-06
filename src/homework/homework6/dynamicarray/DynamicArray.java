472d3c2af3b38227dc2a88011342a6064277d506
AboGhazaryan <abo.ghazaryan@mail.ru>
2025-10-06 14:08:01 +0400

    homework6.correction

--- src/homework/homework6/dynamicarray/DynamicArray.java
+++ src/homework/homework6/dynamicarray/DynamicArray.java
@@ -3,14 +3,14 @@ package homework.homework6.dynamicarray;
 public class DynamicArray {
 
     //սա մեր հիմնական մասիվն է, որտեղ պահելու ենք ավելացվող էլեմենտները
-    int[] array = new int[10];
+    private int[] array = new int[10];
 
     //սա մեր մասիվի մեջ ավելացված էլեմենտների քանակն է
-    int size = 0;
+    private int size = 0;
 
     //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
     //և ավելացնենք
-    void add(int value) {
+    public void add(int value) {
         if (size == array.length) {
             extend();
         }
@@ -22,7 +22,7 @@ public class DynamicArray {
     //2․ քցել հին մասիվի էլեմենտները նորի մեջ
     //3․ հին մասիվի հղումը կապենք նոր մասիվի հղման հետ։
 
-    void extend() {
+   private void extend() {
         int[] newArray = new int[array.length + 10];
         for (int i = 0; i < array.length; i++) {
             newArray[i] = array[i];
@@ -32,7 +32,7 @@ public class DynamicArray {
 
     //եթե տրված ինդեքսը մեր ունեցած մասիվի ինդեքսի սահմաններում է, վերադարձնել
     // մասիվի index-երրորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
-    int getByIndex(int index) {
+    public int getByIndex(int index) {
         if (index < 0 || index > size) {
             return -1;
         }
@@ -40,7 +40,7 @@ public class DynamicArray {
     }
 
     //տպել մասիվի ավելացված էլեմենտները
-    void print() {
+   public void print() {
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
         }
